9f8e27deb657dee7812524b7d2ee088e9de883d9
David Jones <s2986288@staff.ad.griffith.edu.au>
2020-01-31 07:01:22 +1000

    Added onedive process
    The HTML and JS files work with Mammoth to integrate the conversion into Blackboard of Word docs

---
+++ mammoth/onedrive.js
@@ -0,0 +1,520 @@
+/*
+ * onedrive.js
+ * - Code to grab content of a Word doc from OneDrive 
+ * - Used in conjunction with Mammoth
+*/
+
+
+// Config variables
+// TODO - update the clientId to proper final id
+ 
+ 
+
+function loggerCallback(logLevel, message, containsPii) {
+    console.log(" HELLO LOGGER");
+   console.log(message);
+}
+
+var msalConfig = {
+    auth: {
+        //clientId: "cd3eae92-4f64-457e-9bfc-a2a441920144",
+        clientId: "4e260176-5f5d-4a11-b419-8920aff4b13f",
+        authority: "https://login.microsoftonline.com/common"
+    },
+    cache: {
+        cacheLocation: "localStorage",
+        //cacheLocation: "sessionStorage",
+        storeAuthStateInCookie: true
+    },
+    system: {
+        /*logger: new Msal.Logger(
+            loggerCallback, {
+            level: Msal.LogLevel.Verbose,
+            piiLoggingEnabled: false,
+            correlationId: '1234' }
+            )*/
+    }
+};
+
+// Version that works with a path
+//var graphMeEndpoint = "https://graph.microsoft.com/v1.0/me/drive/root:";
+// Works with a shared URL (id)
+// BUT - also need to append /driveItem to the end after the share URL is added
+var graphMeEndpoint = "https://graph.microsoft.com/v1.0/shares/";
+
+
+// this can be used for login or token request, however in more complex situations
+// this can have diverging options
+var requestObj = {
+    scopes: ["user.read","Files.Read.All"]
+};
+//console.log("-------------------------------------");
+//console.log("About to create userAgentApplication");
+var myMSALObj = new Msal.UserAgentApplication(msalConfig);
+    // Register Callbacks for redirect flow
+myMSALObj.handleRedirectCallback(authRedirectCallBack);
+
+//console.log(myMSALObj);
+
+function signIn(myMSALObj,path) {
+
+    myMSALObj.loginPopup(requestObj).then(function (loginResponse) {
+        //Login Success
+        //console.log("ADD EVENT authenticated");
+        acquireTokenPopupAndCallMSGraph(path);
+    }).catch(function (error) {
+        console.log(error);
+    });
+}
+
+//------------------------------------------------------------------------------------
+// Main driver function to load a file from onedrive given the provided path
+
+function loadOneDriveFile( path ) {
+    // update the path to add the API and etc
+    // - for shared URLs
+    path = graphMeEndpoint + path + "/driveItem";
+    
+	// Browser check variables
+	var ua = window.navigator.userAgent;
+	var msie = ua.indexOf('MSIE ');
+	var msie11 = ua.indexOf('Trident/');
+	var msedge = ua.indexOf('Edge/');
+	var isIE = msie > 0 || msie11 > 0;
+	var isEdge = msedge > 0;
+	
+	//If you support IE, our recommendation is that you sign-in using Redirect APIs
+	//If you as a developer are testing using Edge InPrivate mode, please add "isEdge" to the if check
+	// can change this to default an experience outside browser use
+	var loginType = isIE ? "REDIRECT" : "POPUP";
+    
+    // check to see if there's a cached account
+    if (myMSALObj.getAccount()) {
+        addEvent("Got cached token");
+           acquireTokenPopupAndCallMSGraph(path);
+    } else {
+        addEvent("Authenticating");
+        myMSALObj.loginPopup(requestObj).then(function (loginResponse) {
+        //Login Success
+    //    showWelcomeMessage();
+        
+            console.log("ADD EVENT authenticated");
+           acquireTokenPopupAndCallMSGraph(path);
+        }).catch(function (error) {
+            console.log(error);
+            return false;
+        });
+    }
+    
+    return false;
+    
+    //console.log(myMSALObj);
+	if (loginType === 'POPUP') {
+	    
+		if (myMSALObj.getAccount()) {// avoid duplicate code execution on page load in case of iframe and popup window.
+	//		console.log("acount got popup");
+			//showWelcomeMessage();
+			acquireTokenPopupAndCallMSGraph(path);
+		} else {
+		    console.log("Couldn't myMSALObj.getAccount");
+		}
+	}
+	else if (loginType === 'REDIRECT') {		
+	  
+		/*document.getElementById("btnCopy").onclick = function () {			
+			myMSALObj.loginRedirect(requestObj);
+		};*/
+		if (myMSALObj.getAccount() && !myMSALObj.isCallback(window.location.hash)) {// avoid duplicate code execution on page load in case of iframe and popup window.
+		//	console.log("about to show welcome message");
+			acquireTokenRedirectAndCallMSGraph(path);
+		} else {
+		    console.log("Redirect didn't work");
+		}
+	} else {
+		console.error('Please set a valid login type');
+	}
+}
+
+// Make sure we have the necessary Javascript code from Microsoft
+function dynamicallyLoadScript() {
+    var script = document.createElement("script");
+    script.src = "https://cdnjs.cloudflare.com/ajax/libs/bluebird/3.3.4/bluebird.min.js";  
+    document.head.appendChild(script);  
+	//script.src = "https://secure.aadcdn.microsoftonline-p.com/lib/1.0.0/js/msal.js";
+	script.src = "https://alcdn.msauth.net/lib/1.1.3/js/msal.js";
+	document.head.appendChild(script);
+}
+
+// CORS request
+
+var createCORSRequest = function(method, url, accessToken) {
+  var xhr = new XMLHttpRequest();
+  
+  if ("withCredentials" in xhr) {
+    // Most browsers.
+    xhr.open(method, url, true);
+    xhr.responseType='arraybuffer';
+    //xhr.setRequestHeader('Authorization', 'Bearer ' + accessToken);
+  } else if (typeof XDomainRequest != "undefined") {
+    // IE8 & IE9
+    xhr = new XDomainRequest();
+    xhr.open(method, url);
+  } else {
+    // CORS not supported.
+    xhr = null;
+  }
+  return xhr;
+};
+
+
+
+
+
+
+function acquireTokenPopupAndCallMSGraph(path) {
+    //console.log("POPUP");
+    //Always start with acquireTokenSilent to obtain a token in the signed in user from cache
+    myMSALObj.acquireTokenSilent(requestObj).then(function (tokenResponse) {
+        //console.log("ADD EVENT - request access to shared Word doc");
+        addEvent("Obtained token");
+            callMSGraph(path, tokenResponse.accessToken, graphAPICallback);
+    }).catch(function (error) {
+        console.log("ADD EVENT - error requesting token - " + error);
+        addEvent("Error accessing Word document " + error, true);
+        // Call acquireTokenPopup(popup window)
+        if (requiresInteraction(error.errorCode)) {
+            myMSALObj.acquireTokenPopup(requestObj).then(function (tokenResponse) {
+                addEvent("Obtained access to token");
+                callMSGraph(path, tokenResponse.accessToken, graphAPICallback);
+                    //graphMeEndpoint.concat(path), tokenResponse.accessToken, graphAPICallback);
+            }).catch(function (error) {
+                console.log(error);
+            });
+        }
+    });
+}
+
+
+function graphAPICallback(data) {
+    //console.log("Hello call back");
+    document.getElementById("json").innerHTML = JSON.stringify(data, null, 2);
+    //console.log( JSON.stringify(data));
+}
+
+
+function showWelcomeMessage() {
+    var divWelcome = document.getElementById('WelcomeMessage');
+    divWelcome.innerHTML = 'Welcome ' + myMSALObj.getAccount().userName + "to Microsoft Graph API";
+    var loginbutton = document.getElementById('SignIn');
+    loginbutton.innerHTML = 'Sign Out';
+    loginbutton.setAttribute('onclick', 'signOut();');
+}
+
+
+//This function can be removed if you do not need to support IE
+function acquireTokenRedirectAndCallMSGraph(path) {
+     //Always start with acquireTokenSilent to obtain a token in the signed in user from cache
+     myMSALObj.acquireTokenSilent(requestObj).then(function (tokenResponse) {
+         addEvent("Obtained access to token");
+         callMSGraph(path, tokenResponse.accessToken, graphAPICallback);
+     }).catch(function (error) {
+        addEvent("Error accessing Word document " + error, true);
+         // Upon acquireTokenSilent failure (due to consent or interaction or login required ONLY)
+         // Call acquireTokenRedirect
+         if (requiresInteraction(error.errorCode)) {
+             myMSALObj.acquireTokenRedirect(requestObj);
+         }
+     });
+ }
+
+
+function authRedirectCallBack(error, response) {
+    if (error) {
+        console.log(error);
+    }
+    else {
+        if (response.tokenType === "access_token") {
+            callMSGraph(graphConfig.graphEndpoint, response.accessToken, graphAPICallback);
+        } else {
+            console.log("token type is:" + response.tokenType);
+        }
+    }
+}
+
+function requiresInteraction(errorCode) {
+    if (!errorCode || !errorCode.length) {
+        return false;
+    }
+    return errorCode === "consent_required" ||
+        errorCode === "interaction_required" ||
+        errorCode === "login_required";
+}
+
+
+//*******************************************************************************
+// Call the graph
+
+function callMSGraph(theUrl, accessToken, callback) {
+    // Get the microsoft.graph.downloadUrl object
+    
+    addEvent("Requesting Word doc info");
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function () {
+        if (this.readyState == 4 ) {
+            
+            // Success means that we information about the shared file
+            // Including the downloadUrl
+            if ( this.status == 200) {
+                addEvent("Obtained Word doc info")
+                
+            //
+            //callback(JSON.parse(this.responseText));
+            r = JSON.parse(this.responseText);
+            downloadUrl = r['@microsoft.graph.downloadUrl'];
+            
+            getTheFileCORS( downloadUrl, accessToken, graphAPICallback);
+            } else {
+                console.log("readyState " + this.readyState);
+                addEvent("Error obtaining Word doc info", true);
+            }
+        }
+    };
+    // request the information about the file
+    //console.log("going to get " + theUrl + "access token " + accessToken);
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous
+    xmlHttp.setRequestHeader('Authorization', 'Bearer ' + accessToken);
+    xmlHttp.send();
+}
+
+// now get the content of the file
+
+function getTheFileCORS(theUrl, accessToken, callback) {
+    addEvent("Requesting contents of Word doc")
+    
+    var xhr = createCORSRequest('GET', theUrl, accessToken);
+    
+    xhr.onload= function() {
+            var contentType = this.getResponseHeader("Content-Type");
+            
+            //callback( contentType );
+            addEvent("Obtained Word doc contents");
+            
+            // pass the array buffer to mammoth
+            doMammoth( xhr.response );
+    }
+    
+    xhr.onerror = function() {
+            addEvent("Error obtaing Word doc contents");
+    }
+    
+   // xhr.setRequestHeader('Authorization', 'Bearer ' + accessToken);
+    xhr.send();
+}
+
+//------------------------------------------------------------
+// doMammoth
+
+function doMammoth( wordContent ) {
+    
+    var options = {
+        styleMap: [
+            "p[style-name='Section Title'] => h1:fresh",
+                    "p[style-name='Quote'] => blockquote",
+                    "p[style-name='Quotations'] => blockquote",
+                    "p[style-name='Quotation'] => blockquote",
+                    "p[style-name='Body Text'] => p:fresh",
+                    "p[style-name='Text'] => p:fresh",
+                    "p[style-name='Default'] => p:fresh",
+                    "p[style-name='Normal (Web)'] => p:fresh",
+                    "p[style-name='Normal'] => p:fresh",
+                    "p[style-name='Text body'] => p:fresh",
+                    "p[style-name='Textbody1'] => p:fresh",
+                    "p[style-name='Picture'] => div.picture",
+                    "p[style-name='Picture Right'] => div.pictureRight",
+                    "p[style-name='PictureRight'] => div.pictureRight",
+                    
+                    "p[style-name='Video'] => div.video",
+                    "r[style-name='Checkbox Char'] => span.checkbox",
+                    "p[style-name='Checkbox'] => span.checkbox",
+                    "p[style-name='ActivityTitle'] => div.activity > h2:fresh",
+                    "p[style-name='Activity Title'] => div.activity > h2:fresh",
+                    "p[style-name='ActivityText'] => div.activity > div.instructions > p:fresh",
+                    "p[style-name='Activity Text'] => div.activity > div.instructions > p:fresh",
+                    "p[style-name='Activity'] => div.activity > div.instructions > p:fresh",
+                    /*"p[style-name='Activity'] => span.activity",*/
+                    "p[style-name='Bibliography'] => div.apa > p:fresh",
+                    "p[style-name='Reading'] => div.reading > div.instructions > p:fresh",
+                    "p[style-name='Title'] => div.invisible",
+                    "p[style-name='Card'] => div.gu_card",
+                    "r[style-name='Emphasis'] => em:fresh",
+                    "p[style-name='Timeout'] => span.timeout",
+                    "p[style-name='Embed'] => span.embed",
+                    "p[style-name='Note'] => div.ael-note > div.instructions > p:fresh",
+                    /* Adding cards */
+                    "p[style-name='Blackboard Card'] => div.bbCard:fresh",
+                    /* Blackboard item conversion */
+                    "p[style-name='Blackboard Item Heading'] => h1.blackboard",
+                    "p[style-name='Blackboard Item Heading 2'] => h2.blackboard",
+                    "r[style-name='Blackboard Item Link'] => span.blackboardLink",
+                    "p[style-name='Blackboard Item Link'] => span.blackboardlink",
+                    "r[style-name='Blackboard Item Link Char'] => span.blackboardLink",
+                    "r[style-name='Blackboard Content Link'] => span.blackboardContentLink",
+                    "r[style-name='Blackboard Menu Link'] => span.blackboardMenuLink",
+                    /* tables?? */
+                    "r[style-name='small'] => span.smallText",
+                    "r[style-name='StrongCentered'] => span.strongCentered",
+                    "r[style-name='Centered'] => span.centered"
+            ]
+    };
+            
+    addEvent("Starting conversion to HTML")
+    mammoth.convertToHtml({arrayBuffer: wordContent},options)
+        .then(displayResult)
+        .done();
+}
+
+//---------------
+// display the result
+
+function displayResult(result) {
+    addEvent("HTML conversion complete");
+    
+    // add HTML for the content interface
+    result.value = '<div id="GU_ContentInterface">' + result.value + '</div>';
+    document.getElementById("output").innerHTML = "<div id='html'>" + result.value + "</div>";
+    
+    if ( result.messages.length>0){
+        addEvent( result.messages.length + " messages from conversion", false, true);
+    } else {
+        jQuery("#gu_messages").hide();
+        return;
+    }
+        
+    // create and display the messages
+    var messageHtml = result.messages.map(function(message) {
+        return '<li class="' + message.type + '">' + escapeHtml(message.message) + "</li>";
+    }).join("");
+        
+    messageHtml = "<ul>" + messageHtml + "</ul>";
+        
+    document.getElementById("json").innerHTML = messageHtml;
+}
+
+function escapeHtml(value) {
+        return value
+            .replace(/&/g, '&amp;')
+            .replace(/"/g, '&quot;')
+            .replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;');
+}
+
+function updateClipboard(newClip) {
+  navigator.clipboard.writeText(newClip).then(function() {
+    console.log("CLIPBOARD UPDATED");
+  }, function() {
+    console.log("CLIPBOARD FAIL");
+  });
+}
+
+//***************************************
+
+function getCopyElement( title, titleNum) {
+    var element;
+    
+    var gu_ci = jQuery( "<div></div>", {id:"GU_ContentInterface"});
+    
+    // will need to add GU_Interface div around the outside
+    // of the title/titleNum elements
+    if ( title !== null ) {
+        console.log("Get title " + title);
+    } else if ( titleNum !== null) {
+        console.log("get TitleNum " + titleNum);
+        var start = jQuery("#output").find("div.invisible").eq(titleNum-1);
+        console.log(start);
+        var end = jQuery("#output").find("div.invisible").eq(titleNum);
+        element = jQuery(start).nextUntil(end);
+        console.log("------ start ");
+        console.log(jQuery(start).html());
+        console.log("------ end ");
+        console.log(jQuery(end).html());
+        
+        console.log("------element -- length is "+ jQuery(element).length );
+        
+    //    jQuery(element).wrap( '<div id="GU_ContentInterface"></div>');
+        jQuery(gu_ci).append(element);
+        //gu_ci = $('<div>').append($('#GU_ContentInterface').clone()).html();
+        console.log("------element -- length is "+ jQuery(element).length );
+        jQuery(gu_ci).each( function() {
+            console.log(jQuery(this).html());
+        });
+        
+        console.log(gu_ci);
+        console.log(gu_ci.get(0).outerHTML );
+        return jQuery(gu_ci).get();
+    } else {
+        console.log("Getting default");
+        element = document.getElementById("output")
+    }
+    element = document.getElementById("output")
+    return element;
+}
+//
+
+function copyToClipboard(elem) {
+    //console.log("copy to clipboard starting for " + elem);
+    
+    //var copyText = document.getElementById("htmlContent");
+    //console.log(copyText);
+    //console.log("finished");
+    //copyText.select();
+    //document.execCommand("copy");
+    
+    console.log("starting copy to clipboard");
+    console.log(elem);
+    var targetId = "_hiddenCopyText_";
+    var isInput = elem.tagName === "INPUT" || elem.tagName === "TEXTAREA";
+    var origSelectionStart, origSelectionEnd;
+    if (isInput) {
+        target = elem;
+        origSelectionStart = elem.selectionStart;
+        origSelectionEnd = elem.selectionEnd;
+    } else {
+        target = document.getElementById(targetId);
+        if (!target) {
+            var target = document.createElement("textarea");
+            target.style.position = "absolute";
+            target.style.left = "-9999px";
+            target.style.top = "0";
+            target.id = targetId;
+            document.body.appendChild(target);
+        }
+        target.textContent = elem.innerHTML;
+    }
+    var currentFocus = document.activeElement;
+    target.focus();
+    target.setSelectionRange(0, target.value.length);
+    var succeed;
+    try {
+        succeed = document.execCommand("copy");
+    } catch (e) {
+        succeed = false;
+    }
+    if (currentFocus && typeof currentFocus.focus === "function") {
+        currentFocus.focus();
+    }
+ 
+    if (isInput) {
+        elem.setSelectionRange(origSelectionStart, origSelectionEnd);
+    } else {
+        target.textContent = "";
+    }
+    return succeed; 
+}
+
+/**
+ * Sign out the user
+ */
+ function signOut() {
+     myMSALObj.logout();
+ }
+
