dc6535cf54ada65d3947b46c4d037db111ecc41b
David Jones <davidthomjones@gmail.com>
2020-08-04 08:28:35 +1000

    Added universityDate handling

--- content.js
+++ content.js
@@ -5,6 +5,9 @@
  * - Implement a variety of transformations
  */
  
+// Default dates
+var TERM = "3191", YEAR = 2019;
+
 // Wrap arounds for various types of activity 
 var READING=`<div class="image"><img src="https://filebucketdave.s3.amazonaws.com/banner.js/images/icons8-reading-48.png" /></div>`;
 var ACTIVITY=`<div class="image"><img src="https://filebucketdave.s3.amazonaws.com/banner.js/images/icons8-dancing-48.png" /></div>`;
@@ -83,6 +86,8 @@ function contentInterface($){
     jQuery("h2.blackboard").each( handleBlackboardItem );
     jQuery("span.blackboardContentLink").each( handleBlackboardContentLink );
     jQuery("span.blackboardMenuLink").each( handleBlackboardMenuLink );
+
+    jQuery("span.universityDate").each( handleUniversityDate );
     
  	// Add a <div> </div> around all the content following H1s
  	jQuery('#GU_ContentInterface h1').each(function() {
@@ -92,10 +97,10 @@ function contentInterface($){
  	// Add divs around the h2 headings, until h2 or h1
  	jQuery('#GU_ContentInterface h2').each(function() {
  	   // console.log( "Heading text " + jQuery(this).html());
- 	    jQuery(this).nextUntil('h1,h2').addBack().wrapAll('<div class="accordion"></div>');
- 	    jQuery(this).nextUntil('h1,h2').wrapAll('<div></div>');
-     });
-     
+            jQuery(this).nextUntil('h1,h2').addBack().wrapAll('<div class="accordion"></div>');
+            jQuery(this).nextUntil('h1,h2').wrapAll('<div></div>');
+        });
+        
 
     // Update all the readings and activities
     jQuery("div.activity").prepend(ACTIVITY);
@@ -1020,3 +1025,299 @@ function parse_parameters(cmdline) {
     }
     return args;
 }
+
+/*****************************************************************
+ * handleUniversityDate( this )
+ * - given a span with a university date in the form
+ *         _day_ Week _num_
+ *   e.g.   Wednesday Week 5
+ * - update the element to add the actual date based on the
+ *   trimester/study period associated with the course site
+ *   e.g.    Wednesday Week 5 (25th April 2020)
+ */
+
+function handleUniversityDate() {
+    dateText = jQuery(this).text()
+
+    // extract the day and week
+    // Wednesday Week 5 becomes
+    // - day = Wednesday
+    // - week = 5
+    // and convert it to a date string
+    //  date = March 12, 2019
+    var day = '', week='', date='';
+    m = dateText.match(
+        /.*\b(((mon|tues|wed(nes)?|thur(s)?|fri|sat(ur)?|sun)(day)?))\b *week *\b([0-9]*)/i);
+    if (m) {
+        day = m[1];
+        week  = m[m.length-1];
+        date = getTermDate(week, day);
+    } else {
+        // couldn't match the date, finish up
+        return false;
+    }
+
+    // update the HTML item
+    dateText = dateText + " (" + date + ")";
+    jQuery(this).html(dateText);
+}
+
+//*********************
+// getTermDate( week, day )
+// - given a week and a day of Griffith semester return actual
+//   date for matching that study period
+// - weeks start on Monday
+
+function getTermDate(week, dayOfWeek = 'Monday') {
+
+    dayOfWeek = dayOfWeek.toLowerCase()
+    var start;
+
+    // if the week is not within the term return empty string
+    if (typeof TERM_DATES[TERM][week] === 'undefined') {
+        return "";
+    }
+
+    // else calculate the date and generate usable string
+    start = TERM_DATES[TERM][week].start;
+    var d = new Date(start);
+
+    // if dayOfWeek is not Monday, add some days to the start of the week
+    if ( dayOfWeek !== 'monday') {
+        var dayToNum = { 'tuesday' : 1, 'wednesday': 2, 'thursday':3, 'friday':4, 'saturday': 5, 'sunday': 6 };
+        if ( dayOfWeek in dayToNum ) { 
+            d.setDate( d.getDate() + dayToNum[dayOfWeek.toLowerCase()]);
+        }
+    }
+    // generate string from date with given options
+    const options = { weekday:undefined, year:'numeric', month:'long', day:'numeric'};
+    dateString = d.toLocaleDateString(undefined, options);
+
+    return dateString;
+}
+
+var TERM_DATES = {
+    // OUA 2020 Study Period 1
+    "2201": {
+        "0": { "start": "2020-02-24", "stop": "2020-03-01" },
+        "1": { "start": "2020-03-02", "stop": "2020-03-08" },
+        "2": { "start": "2020-03-09", "stop": "2020-03-15" },
+        "3": { "start": "2020-03-16", "stCop": "2020-03-22" },
+        "4": { "start": "2020-03-23", "stop": "2020-03-29" },
+        "5": { "start": "2020-03-30", "stop": "2020-04-05" },
+        "6": { "start": "2020-04-06", "stop": "2020-04-12" },
+        "7": { "start": "2020-04-13", "stop": "2020-04-19" },
+        "8": { "start": "2020-04-20", "stop": "2020-04-26" },
+        "9": { "start": "2020-04-27", "stop": "2020-05-03" },
+        "10": { "start": "2020-05-04", "stop": "2020-05-10" },
+        "11": { "start": "2020-05-11", "stop": "2020-05-17" },
+        "12": { "start": "2020-05-18", "stop": "2020-05-24" },
+        "13": { "start": "2020-05-25", "stop": "2020-05-31" },
+        "14": { "start": "2020-06-01", "stop": "2020-06-05" },
+        /* End of study period 4 */
+        "exam": { "start": "2020-06-01", "stop": "2020-06-05" },
+        // No exam ?? "exam" : { "start": "2019-10-10", "stop" : "2019-10-19" }
+    },
+    // OUA 2020 Study Period 2
+    "2203": {
+        "0": { "start": "2020-05-25", "stop": "2020-05-31" },
+        "1": { "start": "2020-06-01", "stop": "2020-06-07" },
+        "2": { "start": "2020-06-08", "stop": "2020-06-14" },
+        "3": { "start": "2020-06-15", "stop": "2020-06-21" },
+        "4": { "start": "2020-06-22", "stop": "2020-06-28" },
+        "5": { "start": "2020-06-29", "stop": "2020-07-05" },
+        "6": { "start": "2020-07-06", "stop": "2020-07-12" },
+        "7": { "start": "2020-07-13", "stop": "2020-07-19" },
+        "8": { "start": "2020-07-20", "stop": "2020-07-26" },
+        "9": { "start": "2020-07-27", "stop": "2020-08-02" },
+        "10": { "start": "2020-08-03", "stop": "2020-08-09" },
+        "11": { "start": "2020-08-10", "stop": "2020-05-17" },
+        "12": { "start": "2020-08-17", "stop": "2020-05-24" },
+        "13": { "start": "2020-08-24", "stop": "2020-05-31" },
+        "14": { "start": "2020-08-31", "stop": "2020-09-06" },
+        /* End of study period 4 */
+        "exam": { "start": "2020-08-31", "stop": "2020-09-04" },
+        // No exam ?? "exam" : { "start": "2019-10-10", "stop" : "2019-10-19" }
+    },
+    // OUA 2020 Study Period 3
+    "2205": {
+        "0": { "start": "2020-08-31", "stop": "2020-09-06" },
+        "1": { "start": "2020-09-07", "stop": "2020-09-13" },
+        "2": { "start": "2020-09-14", "stop": "2020-09-20" },
+        "3": { "start": "2020-09-21", "stop": "2020-09-27" },
+        "4": { "start": "2020-09-28", "stop": "2020-10-04" },
+        "5": { "start": "2020-10-05", "stop": "2020-10-11" },
+        "6": { "start": "2020-10-12", "stop": "2020-10-19" },
+        "7": { "start": "2020-10-19", "stop": "2020-10-25" },
+        "8": { "start": "2020-10-26", "stop": "2020-11-01" },
+        "9": { "start": "2020-11-02", "stop": "2020-11-08" },
+        "10": { "start": "2020-11-09", "stop": "2020-11-15" },
+        "11": { "start": "2020-11-16", "stop": "2020-11-22" },
+        "12": { "start": "2020-11-23", "stop": "2020-11-29" },
+        "13": { "start": "2020-11-30", "stop": "2020-12-06" },
+        "14": { "start": "2020-12-07", "stop": "2020-12-13" },
+        /* End of study period 4 */
+        "exam": { "start": "2020-12-07", "stop": "2020-12-13" },
+        // No exam ?? "exam" : { "start": "2019-10-10", "stop" : "2019-10-19" }
+    },
+    // OUA 2020 Study Period 4
+    "2207": {
+        "0": { "start": "2020-11-30", "stop": "2020-12-06" },
+        "1": { "start": "2020-12-07", "stop": "2020-12-13" },
+        "2": { "start": "2020-12-14", "stop": "2020-12-20" },
+        "3": { "start": "2020-12-21", "stop": "2020-12-27" },
+        "4": { "start": "2020-12-28", "stop": "2021-01-03" },
+        "5": { "start": "2021-01-04", "stop": "2021-01-10" },
+        "6": { "start": "2021-01-11", "stop": "2021-01-17" },
+        "7": { "start": "2021-01-18", "stop": "2021-01-24" },
+        "8": { "start": "2021-01-25", "stop": "2021-01-31" },
+        "9": { "start": "2021-02-01", "stop": "2021-02-07" },
+        "10": { "start": "2021-02-08", "stop": "2021-02-14" },
+        "11": { "start": "2021-02-15", "stop": "2021-02-21" },
+        "12": { "start": "2021-02-22", "stop": "2021-02-28" },
+        "13": { "start": "2021-03-01", "stop": "2021-03-07" },
+        "14": { "start": "2021-03-08", "stop": "2021-03-14" },
+        /* End of study period 4 */
+        "exam": { "start": "2021-03-08", "stop": "2021-03-14" },
+        // No exam ?? "exam" : { "start": "2019-10-10", "stop" : "2019-10-19" }
+    },
+    // Griffith 2020 Trimester 2
+    "3205": {
+        "0": { "start": "2020-07-06", "stop": "2020-07-12" },
+        "1": { "start": "2020-07-13", "stop": "2020-07-19" },
+        "2": { "start": "2020-07-20", "stop": "2020-08-26" },
+        "3": { "start": "2020-07-27", "stop": "2020-08-02" },
+        "4": { "start": "2020-08-03", "stop": "2020-08-16" },
+        "5": { "start": "2020-08-17", "stop": "2020-08-23" },
+        "6": { "start": "2020-08-24", "stop": "2020-08-30" },
+        "7": { "start": "2020-08-31", "stop": "2020-09-06" },
+        "8": { "start": "2020-09-07", "stop": "2020-09-13" },
+        "9": { "start": "2020-09-14", "stop": "2020-09-20" },
+        "10": { "start": "2020-09-21", "stop": "2020-09-27" },
+        "11": { "start": "2020-09-28", "stop": "2020-10-04" },
+        "12": { "start": "2020-10-05", "stop": "2020-10-11" },
+        "13": { "start": "2020-10-12", "stop": "2020-10-18" },
+        "14": { "start": "2020-10-19", "stop": "2020-10-25" },
+        "15": { "start": "2020-10-27", "stop": "2020-11-01" },
+        "exam": { "start": "2020-10-12", "stop": "2020-10-18" }
+    },
+    // Griffith 2020 Trimester 1
+    "3201": {
+        "0": { "start": "2020-02-17", "stop": "2020-02-23" },
+        "1": { "start": "2020-02-24", "stop": "2020-03-01" },
+        "2": { "start": "2020-03-02", "stop": "2020-03-08" },
+        "3": { "start": "2020-03-09", "stop": "2020-03-15" },
+        "4": { "start": "2020-03-16", "stop": "2020-03-22" },
+        "5": { "start": "2020-03-23", "stop": "2020-03-29" },
+        "6": { "start": "2020-03-30", "stop": "2020-04-05" },
+        "7": { "start": "2020-04-13", "stop": "2020-04-19" },
+        "8": { "start": "2020-04-20", "stop": "2020-04-26" },
+        "9": { "start": "2020-04-27", "stop": "2020-05-03" },
+        "10": { "start": "2020-05-04", "stop": "2020-05-10" },
+        "11": { "start": "2020-05-11", "stop": "2020-05-17" },
+        "12": { "start": "2020-05-18", "stop": "2020-05-24" },
+        "13": { "start": "2020-05-25", "stop": "2020-05-31" },
+        "exam": { "start": "2020-06-01", "stop": "2020-06-07" }
+    },
+    // Griffith 2019 Trimester 3
+    "3198": {
+        "0": { "start": "2019-10-21", "stop": "2019-10-27" },
+        "1": { "start": "2019-10-28", "stop": "2019-11-03" },
+        "2": { "start": "2019-11-04", "stop": "2019-11-10" },
+        "3": { "start": "2019-11-11", "stop": "2019-11-17" },
+        "4": { "start": "2019-11-18", "stop": "2019-11-24" },
+        "5": { "start": "2019-11-25", "stop": "2019-12-1" },
+        "6": { "start": "2019-12-02", "stop": "2019-12-08" },
+        "7": { "start": "2019-12-09", "stop": "2019-12-15" },
+        "8": { "start": "2019-12-16", "stop": "2019-12-22" },
+        "9": { "start": "2020-01-06", "stop": "2020-01-12" },
+        "10": { "start": "2020-01-13", "stop": "2020-01-19" },
+        "11": { "start": "2020-01-20", "stop": "2020-01-26" },
+        "12": { "start": "2020-01-27", "stop": "2020-02-02" },
+        "13": { "start": "2020-02-03", "stop": "2020-02-09" },
+        "exam": { "start": "2020-02-06", "stop": "2020-02-15" }
+        // No exam ?? "exam" : { "start": "2019-10-10", "stop" : "2019-10-19" }
+    },
+    // OUA Study Period 4 2019
+    "2197": {
+        "0": { "start": "2019-11-18", "stop": "2019-11-24" },
+        "1": { "start": "2019-11-25", "stop": "2019-12-01" },
+        "2": { "start": "2019-12-02", "stop": "2019-12-08" },
+        "3": { "start": "2019-12-09", "stop": "2019-12-15" },
+        "4": { "start": "2019-12-16", "stop": "2019-12-22" },
+        "5": { "start": "2019-12-23", "stop": "2019-09-29" },
+        "6": { "start": "2019-12-30", "stop": "2020-01-05" },
+        "7": { "start": "2020-01-06", "stop": "2020-01-12" },
+        "8": { "start": "2020-01-13", "stop": "2020-01-19" },
+        "9": { "start": "2020-01-20", "stop": "2020-01-26" },
+        "10": { "start": "2020-01-27", "stop": "2020-02-02" },
+        "11": { "start": "2020-02-03", "stop": "2020-02-09" },
+        "12": { "start": "2020-02-10", "stop": "2020-02-16" },
+        "13": { "start": "2019-02-17", "stop": "2020-02-23" },
+        /* End of study period 4 */
+        "14": { "start": "2020-02-24", "stop": "2020-03-01" },
+        "15": { "start": "2020-03-02", "stop": "2020-03-08" },
+        // No exam ?? "exam" : { "start": "2019-10-10", "stop" : "2019-10-19" }
+    },
+    // OUA Study Period 3 2019
+    "2195": {
+        "0": { "start": "2019-08-19", "stop": "2019-09-25" },
+        "1": { "start": "2019-08-26", "stop": "2019-09-01" },
+        "2": { "start": "2019-09-02", "stop": "2019-09-18" },
+        "3": { "start": "2019-09-09", "stop": "2019-09-15" },
+        "4": { "start": "2019-09-16", "stop": "2019-09-22" },
+        "5": { "start": "2019-09-23", "stop": "2019-09-29" },
+        "6": { "start": "2019-09-30", "stop": "2019-10-06" },
+        "7": { "start": "2019-10-07", "stop": "2019-10-13" },
+        "8": { "start": "2019-10-14", "stop": "2019-08-20" },
+        "9": { "start": "2019-10-21", "stop": "2019-10-27" },
+        "10": { "start": "2019-10-28", "stop": "2019-11-03" },
+        "11": { "start": "2019-11-04", "stop": "2019-11-10" },
+        "12": { "start": "2019-11-11", "stop": "2019-11-17" },
+        "13": { "start": "2019-11-18", "stop": "2019-11-24" },
+        /* End of study period 3 */
+        "14": { "start": "2019-11-25", "stop": "2019-12-01" },
+        "15": { "start": "2019-10-07", "stop": "2019-10-13" },
+        // No exam ?? "exam" : { "start": "2019-10-10", "stop" : "2019-10-19" }
+    },
+    // Griffith 2019 Trimester 2
+    "3195": {
+        "0": { "start": "2019-07-01", "stop": "2019-07-07" },
+        "1": { "start": "2019-07-08", "stop": "2019-07-14" },
+        "2": { "start": "2019-07-15", "stop": "2019-07-21" },
+        "3": { "start": "2019-07-22", "stop": "2019-07-28" },
+        "4": { "start": "2019-07-29", "stop": "2019-08-04" },
+        "5": { "start": "2019-08-05", "stop": "2019-08-11" },
+        "6": { "start": "2019-08-19", "stop": "2019-08-25" },
+        "7": { "start": "2019-08-26", "stop": "2019-09-01" },
+        "8": { "start": "2019-09-02", "stop": "2019-09-08" },
+        "9": { "start": "2019-09-09", "stop": "2019-09-15" },
+        "10": { "start": "2019-09-16", "stop": "2019-09-22" },
+        "11": { "start": "2019-09-23", "stop": "2019-09-29" },
+        "12": { "start": "2019-09-30", "stop": "2019-10-06" },
+        "13": { "start": "2019-10-07", "stop": "2019-10-13" },
+        "14": { "start": "2019-10-14", "stop": "2019-10-20" },
+        "15": { "start": "2019-10-21", "stop": "2019-10-27" },
+        "exam": { "start": "2019-10-10", "stop": "2019-10-19" }
+    },
+    "3191": {
+        "0": { "start": "2019-02-18", "stop": "2019-02-24" },
+        "1": { "start": "2019-02-25", "stop": "2019-03-03" },
+        "2": { "start": "2019-03-04", "stop": "2019-03-10" },
+        "3": { "start": "2019-03-11", "stop": "2019-03-17" },
+        "4": { "start": "2019-03-18", "stop": "2019-03-24" },
+        "5": { "start": "2019-03-25", "stop": "2019-03-31" },
+        "6": { "start": "2019-04-01", "stop": "2019-04-07" },
+        "7": { "start": "2019-04-08", "stop": "2019-04-14" },
+        "8": { "start": "2019-04-22", "stop": "2019-04-28" },
+        "9": { "start": "2019-04-29", "stop": "2019-05-05" },
+        "10": { "start": "2019-05-06", "stop": "2019-05-12" },
+        "11": { "start": "2019-05-13", "stop": "2019-05-19" },
+        "12": { "start": "2019-05-20", "stop": "2019-05-26" },
+        "13": { "start": "2019-05-27", "stop": "2019-06-02" },
+        "14": { "start": "2019-06-03", "stop": "2019-06-09" },
+        "15": { "start": "2019-06-10", "stop": "2019-06-17" },
+        "exam": { "start": "2019-05-30", "stop": "2019-06-08" }
+    }
+
+};
